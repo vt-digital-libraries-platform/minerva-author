dc6ddb010992f74dcc88118f2e9a3c68e30d0062
Lee Hunter <whunter@vt.edu>
2025-03-09 14:23:24 -0400

    just port in g

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 ~/miniconda3/bin/conda init bash
 . /root/.bashrc
 cd /home/ubuntu/minerva-author/src
-/root/miniconda3/bin/python -m gunicorn --bind 127.0.0.1:8000 --workers=1 --threads=15 app:app
+/root/miniconda3/bin/python -m gunicorn --bind :8000 --workers=1 --threads=15 app:app
 # /root/miniconda3/bin/python ./src/app.py
